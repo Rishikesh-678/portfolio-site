95ed75bee4248fcefc7fb2b008d249200cac37d6
Rishikesh P <153612432+Rishikesh-678@users.noreply.github.com>
2025-06-11 01:08:02 +0530

    corrected some minor spelling mistakes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-    const menu = documentquerySelector('.menu-links');
-    const icon = documentquerySelector('.hamburger-icon');
-    menu.classlist.toggle('open');
-    icon.classlist.toggle('open');
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
 }
